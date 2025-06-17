3fe621ffd6837d125dc118bdc7c2615b0a7c49e2
Sebas <ju.carmonagduocuc.cl>
2025-06-16 20:15:14 -0400

    Mantenimiento ahora es un directorio rastreado normalmente en el monorepo.

--- Mantenimiento/estadisticas/src/main/java/com/jugenix/estadisticas/repository/EstadisticaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jugenix.estadisticas.repository;
-
-import com.jugenix.estadisticas.model.Estadistica;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstadisticaRepository extends JpaRepository<Estadistica, Long> {
-    // Podrías agregar filtros por tipo o fecha en el futuro
-}
